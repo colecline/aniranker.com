e2c3e6656755e24f4f1b62bf1bfe7f5e597ceeda
Cole Cline <coleclinegh@gmail.com>
2022-04-11 19:12:36 -0500

    Updated Frontend, Added Anime Selection, Moved Pages Around

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
         <>
         <div className="bg-white mb-8 shadow-md">
             <div className="max-w-6xl mx-auto flex justify-center py-3">
-                <h1 className="text-md font-semibold">Anime Character Ranker</h1>
+                <h1 className="text-md font-semibold">aniranker</h1>
             </div>
         </div>
         </>
